4dd665992b5f17c8a5111f28b624a670a2ccadf4
vic-company <1173646430@qq.com>
2018-07-11 09:59:13 +0800

    update pom

--- springboot-websocket-sockjs/src/main/java/com/websocket/sockjs/demo/controller/GreetingController.java
+++ springboot-websocket-sockjs/src/main/java/com/websocket/sockjs/demo/controller/GreetingController.java
@@ -6,7 +6,9 @@ import com.websocket.sockjs.demo.vo.Shout;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class GreetingController {
     //    A1）@MessageMapping注解：表示 handleShout()方法能够处理 指定目的地上到达的消息；
 //    A2）这个目的地（消息发送目的地url）就是 "/server/app/hello"，其中 "/app" 是 隐含的 ,"/server" 是 springmvc 项目名称；
